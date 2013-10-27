068d06a1ff7d0eaf987af5d7d3c280fa435db8f8
Dan <ellisd23@gmail.com>
2013-10-27 14:32:00 -0400

    avg -> sum

--- advice.js
+++ advice.js
@@ -19,34 +19,34 @@ function Advisor(team) {
     this.team = team;
 
     this.advice_carries = function () {
-        carry_avg = team.stat_avg('Carry')
-        if (carry_avg >= 1.2)
+        carry_avg = team.stat_sum('Carry')
+        if (carry_avg >= 6)
             return new Advice('Carries', 'warning', 'You have a lot of carries!');
-        if (carry_avg >= 0.8)
+        if (carry_avg >= 4)
             return new Advice('Carries', 'success', 'You have a good amount of carries.');
-        if (carry_avg >= 0.4)
+        if (carry_avg >= 2)
             return new Advice('Carries', 'warning', 'You are weak on carries.');
         return new Advice('Carries', 'danger', 'You have almost no carry potential.  Finish fast!')
     }
 
     this.advice_disables = function () {
-        disables = team.stat_avg('Disabler');
-        if (disables >= 1)
+        disables = team.stat_sum('Disabler');
+        if (disables >= 5)
             return new Advice('Disables', 'success', 'You have a lot of disables.');
-        if (disables >= 0.8)
+        if (disables >= 4)
             return new Advice('Disables', 'info', 'You have a moderate amount of disables.');
-        if (disables >= 0.4)
+        if (disables >= 2)
             return new Advice('Disables', 'warning', 'You are weak on disables.');
         return new Advice('Disables', 'danger', 'You have no disables!');
     }
 
     this.advice_initiation = function () {
-        initiation = team.stat_avg('Initiator');
-        if (initiation >= 1.2)
+        initiation = team.stat_sum('Initiator');
+        if (initiation >= 6)
             return new Advice('Initiation', 'success', 'Holy jesus you have a lot of initation!');
-        if (initiation >= 0.8)
+        if (initiation >= 4)
             return new Advice('Initiation', 'success', 'You have some solid initiation.');
-        if (initiation >= 0.4)
+        if (initiation >= 2)
             return new Advice('Initiation', 'warning', 'Your initiation is a bit weak.');
         return new Advice('Initiation', 'danger', 'You have almost no initiation!');
     }
@@ -81,10 +81,10 @@ function Advisor(team) {
     }
 
     this.advice_supports = function () {
-        supports = team.stat_avg('Support');
-        if (supports >= 0.6)
+        supports = team.stat_sum('Support');
+        if (supports >= 3)
             return new Advice('Supports', 'success', 'You have a good amount of support.')
-        if (supports >= 0.2)
+        if (supports >= 1)
             return new Advice('Supports', 'warning', 'You are weak on supports.')
         return new Advice('Supports', 'danger', 'You have no supports!')
     }
