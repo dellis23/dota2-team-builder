3af8c0b5e8790f613fffd387ead4cb36ff418082
Dan <dan@dan-M17xR3.(none)>
2013-10-30 11:41:30 -0400

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-git ftp init -u ricknews -p --syncroot _site - ftp://ricknews.net/public_html/dotateam.me
+git ftp push -u ricknews -p --syncroot _site - ftp://ricknews.net/public_html/dotateam.me
 
