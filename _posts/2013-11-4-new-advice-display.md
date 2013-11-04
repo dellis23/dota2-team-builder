21cd34386e965a895df34908967c5f9d7a898895
Dan <dan@dan-M17xR3.(none)>
2013-11-04 12:26:28 -0500

    0.6, new advice bars

---
+++ _posts/2013-11-4-new-advice-display.md
@@ -0,0 +1,7 @@
+---
+layout: update_template
+title: New advice display (v0.6)
+---
+Features:
+
+  * For quantitative advice, colored progress bars are now used.
--- advice.js
+++ advice.js
@@ -10,59 +10,108 @@ function Advice(name, level, message) {
     this.level = level;
     this.message = message;
     this.display = function () {
-       return "<li class='text-{0}'><strong>{1}</strong>: <em>{2}</em></li>"
+       return "<div class='row text-{0}'><div class='col-md-3'><strong>{1}</strong>: </div><div class='col-md-9'><em>{2}</em></div></div>"
               .format(this.level, this.name, this.message)
     }
 }
+
+
+function GenericAdvice(name, ranges, current, optimal) {
+    this.name = name;
+    this.ranges = ranges;
+    /*
+     * ranges = [
+           {min: 0, max: 3, level: 'success'},
+           ...
+       ]
+     */
+    this.current = current;
+    this.optimal = optimal;
+
+    this.display = function () {
+
+        // Calculations
+        for (var i = 0; i < this.ranges.length; i++) {
+            if (this.current >= this.ranges[i].min && this.current <= this.ranges[i].max) {
+                current_level = this.ranges[i].level;
+                break;
+            }
+        }
+        current_percentage = this.current / this.optimal * 100;
+
+        // Render Output
+        return '<div class="row text-{1}"><div class="col-md-3"><strong>{0}</strong>: </div><div class="col-md-9"><div class="progress progress-striped"><div class="progress-bar progress-bar-{1}" style="width: {2}%"></div></div></div></div>'
+               .format(this.name, current_level, current_percentage);
+    }
+}
+
 
 function Advisor(team) {
     this.team = team;
 
     this.advice_carries = function () {
-        carry_avg = team.stat_sum('Carry');
-        if (carry_avg >= 9)
-            return new Advice('Carries', 'warning', 'You have a lot of carries!');
-        if (carry_avg >= 5)
-            return new Advice('Carries', 'success', 'You have a good amount of carries.');
-        if (carry_avg >= 2)
-            return new Advice('Carries', 'warning', 'You are weak on carries.');
-        return new Advice('Carries', 'danger', 'You have almost no carry potentia!')
+        carry_sum = team.stat_sum('Carry');
+        return new GenericAdvice(
+            'Carry',
+            [
+                {min: 0, max: 2, level: 'danger'},
+                {min: 3, max: 4, level: 'warning'},
+                {min: 5, max: 8, level: 'success'},
+                {min: 9, max: 15, level: 'danger'}
+            ],
+            carry_sum,
+            5)
     }
 
     this.advice_disables = function () {
         disables = team.stat_sum('Disabler');
-        if (disables >= 7)
-            return new Advice('Disables', 'success', 'You have a lot of disables.');
-        if (disables >= 4)
-            return new Advice('Disables', 'info', 'You\'re a little low on disables!');
-        if (disables >= 2)
-            return new Advice('Disables', 'warning', 'You are weak on disables!');
-        return new Advice('Disables', 'danger', 'You have no disables!');
+        return new GenericAdvice(
+            'Disables',
+            [
+                {min: 0, max: 2, level: 'danger'},
+                {min: 3, max: 6, level: 'warning'},
+                {min: 7, max: 15, level: 'success'}
+            ],
+            disables,
+            7);
     }
 
     this.advice_initiation = function () {
         initiation = team.stat_sum('Initiator');
-        if (initiation >= 3)
-            return new Advice('Initiation', 'success', 'You have a lot of initation/teamfighting.');
-        if (initiation >= 1)
-            return new Advice('Initiation', 'warning', 'Your initiation is a bit weak.');
-        return new Advice('Initiation', 'danger', 'You have no initiation!');
+        return new GenericAdvice(
+            'Initiation',
+            [
+                {min: 0, max: 1, level: 'danger'},
+                {min: 2, max: 2, level: 'warning'},
+                {min: 3, max: 15, level: 'success'}
+            ],
+            initiation,
+            3);
     }
 
     this.advice_nukers = function () {
         nukers = this.team.stat_sum('Nuker');
-        if (nukers >= 7)
-            return new Advice('Nukers', 'success', "You've got strong nuking potential.");
-        if (nukers >= 4)
-            return new Advice('Nukers', 'info', "You have alright nuking potential.");
-        return new Advice('Nukers', 'warning', "You have almost no nukers!")
+        return new GenericAdvice(
+            'Nukers',
+            [
+                {min: 0, max: 3, level: 'danger'},
+                {min: 4, max: 6, level: 'warning'},
+                {min: 7, max: 15, level: 'success'}
+            ],
+            nukers,
+            7);
     }
 
     this.advice_pushers = function () {
         pushing = this.team.stat_sum('Pusher');
-        if (pushing >= 3)
-            return new Advice('Pushers', 'success', "You've got good tower pushers.")
-        return new Advice('Pushers', 'warning', "You're weak on tower pushers!")
+        return new GenericAdvice(
+            'Pushers',
+            [
+                {min: 0, max: 2, level: 'danger'},
+                {min: 3, max: 15, level: 'success'}
+            ],
+            pushing,
+            3);
     }
 
     this.advice_squishiness = function () {
@@ -78,20 +127,28 @@ function Advisor(team) {
 
     this.advice_supports = function () {
         supports = team.stat_sum('Support');
-        if (supports >= 10)
-            return new Advice('Supports', 'warning', 'Uh oh, that\'s a lot of supports')
-        if (supports >= 6)
-            return new Advice('Supports', 'success', 'You have a nice amount of support.')
-        if (supports >= 1)
-            return new Advice('Supports', 'warning', 'You are weak on supports.')
-        return new Advice('Supports', 'danger', 'You have no supports!')
+        return new GenericAdvice(
+            'Support',
+            [
+                {min: 0, max: 3, level: 'danger'},
+                {min: 4, max: 5, level: 'warning'},
+                {min: 6, max: 9, level: 'success'},
+                {min: 10, max: 15, level: 'danger'}
+            ],
+            supports,
+            6);
     }
 
     this.advice_lane_support = function () {
         lane_support = team.stat_sum('LaneSupport');
-        if (lane_support >= 4)
-            return new Advice('Lane Support', 'success', 'You\'ve got good lane support.')
-        return new Advice('Lane Support', 'warning', 'You don\'t have much lane support ability.');
+        return new GenericAdvice(
+            'Support',
+            [
+                {min: 0, max: 3, level: 'danger'},
+                {min: 4, max: 15, level: 'success'}
+            ],
+            lane_support,
+            4);
     }
 
     this.advice_junglers = function () {
@@ -101,16 +158,16 @@ function Advisor(team) {
         return false;
     }
 
-    this.get_all = function () {
-        pieces_of_advice = [];
+    this.display = function () {
+        output = '';
         for (func in this) {
             if (func.indexOf("advice_") == 0) {
                 adv = this[func]();
                 if (adv) {
-                    pieces_of_advice.push(adv);
+                    output += adv.display();
                 }
             }
         }
-        return pieces_of_advice;
+        return output;
     }
 }
